e8b336a925362743e8b5c1b8028d5ddcdbee9556
Raymond Lam <raymond.m.lam@gmail.com>
2022-10-11 13:37:03 +0200

    fixed types issue with clsx and button is reacting to isFluid

--- components/BaseButton/BaseButton.tsx
+++ components/BaseButton/BaseButton.tsx
@@ -1,14 +1,11 @@
 import { IButtonProps } from './interface'
+import Button from './Button.module.scss'
+import clsx from 'clsx'
 
-const BaseButton = ({
-  text,
-  href,
-  target,
-  size,
-  isFluid,
-  as
-}: IButtonProps) => {
-  return <button>{text}</button>
+const BaseButton = ({ text, href, target, size, isFluid }: IButtonProps) => {
+  const fullWidth = isFluid ? `${Button.fullWidth}` : ''
+
+  return <button className={clsx(fullWidth)}>{text}</button>
 }
 
 export default BaseButton
--- components/BaseButton/interface.ts
+++ components/BaseButton/interface.ts
@@ -1,10 +1,9 @@
 import React from 'react'
 
-export interface IButtonProps extends React.ComponentProps<"button"> {
-    text: string
-    href?: string
-    target?: string
-    size: "s" | "m"
-    isFluid?: boolean
-    as: "a" | "button"
+export interface IButtonProps extends React.ComponentProps<'button'> {
+  text: string
+  href?: string
+  target?: string
+  size?: 's' | 'm'
+  isFluid?: boolean
 }
