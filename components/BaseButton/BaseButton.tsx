383c0f903f618a4373dfa1e9839c405c0affaf93
Raymond Lam <raymond.m.lam@gmail.com>
2022-10-11 14:00:07 +0200

    btn rendering size

--- components/BaseButton/BaseButton.tsx
+++ components/BaseButton/BaseButton.tsx
@@ -2,8 +2,20 @@ import { IButtonProps } from './interface'
 import styles from './Button.module.scss'
 
 const BaseButton = ({ text, href, target, size, isFluid }: IButtonProps) => {
+  const fullWidth = isFluid ? `${styles.fullWidth}` : ''
+  const btnSize =
+    size === 'm'
+      ? `${styles.buttonMedium}`
+      : size === 's'
+      ? `${styles.buttonSmall}`
+      : ''
+
   return href ? (
-    <a href={href} className={styles.button} target={target}>
+    <a
+      href={href}
+      className={`${styles.button} ${fullWidth} ${btnSize}`}
+      target={target}
+    >
       {text}
     </a>
   ) : (
--- pages/ButtonPage.tsx
+++ pages/ButtonPage.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 const ButtonPage: NextPage = () => {
   return (
     <div className={styles.flexContainer}>
-      <BaseButton href="/" text="Hello" />
+      <BaseButton href="/" text="Hello" size="m" />
     </div>
   )
 }
